7902696801a8547038445bb31483a9bcb1a7ba42
jhun-crypto <nksenzov684@gmail.com>
2025-06-17 19:53:46 +0500

    Delete s directory

--- s/react-app/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import AppWrapper from './App'; // Обрати внимание, что теперь импортируем AppWrapper
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <AppWrapper />
-  </React.StrictMode>,
-);
